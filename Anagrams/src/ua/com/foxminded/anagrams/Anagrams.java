0f55180ed93637b5ea22b526ac26c62296d4e304
Maksim Khitrov <khitrov.maks@gmail.com>
2020-03-26 19:04:55 +0300

    initial commit

---
+++ Anagrams/src/ua/com/foxminded/anagrams/Anagrams.java
@@ -0,0 +1,11 @@
+package ua.com.foxminded.anagrams;
+
+public class Anagrams {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Reverse.reverseOnlyLetters("H!!!ello"));
+
+	}
+	
+}
---
+++ Anagrams/test/ua/com/foxminded/anagrams/ReverseTest.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded.anagrams;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReverseTest {
+
+	@Test
+	public void test() {
+		assertEquals("o!!!lleH", Reverse.reverseOnlyLetters("H!!!ello"));
+	}
+
+}

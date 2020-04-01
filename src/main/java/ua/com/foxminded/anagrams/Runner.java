4b5230643c44e53623131ae63e603279218fd482
Maksim Khitrov <khitrov.maks@gmail.com>
2020-04-01 14:33:43 +0300

    done latest requirements

---
+++ src/main/java/ua/com/foxminded/anagrams/Runner.java
@@ -0,0 +1,11 @@
+package ua.com.foxminded.anagrams;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Anagram reverse = new Anagram();
+
+        System.out.println(reverse.reverseOnlyLetters("H!!!ello"));
+    }
+
+}
---
+++ src/test/java/ua/com/foxminded/anagrams/AnagramTest.java
@@ -0,0 +1,16 @@
+package ua.com.foxminded.anagrams;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnagramTest {
+
+    @Test
+    public void test() {
+        Anagram reverse = new Anagram();
+
+        assertEquals("o!!!lleH", reverse.reverseOnlyLetters("H!!!ello"));
+    }
+
+}
